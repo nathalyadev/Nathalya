c29c4461652ac6f33d7b6631f2a53723bc26ce76
nathgit198 <nath.conta14@gmail.com>
2022-04-26 20:14:47 -0300

    Novas modificações

--- script.js
+++ script.js
@@ -68,3 +68,35 @@ window.onscroll = () => {
         scrollTop.classList.remove('btn-scroll-visible')
     }
 }
+
+/* Slider */
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+/* IMG */
+/* function mostrarTexto() {
+    document.getElementById("test").innerHTML = "meu texto";
+    console.log(mostrarTexto)
+}
+function ocultarTexto() {
+    document.getElementById("test").innerHTML = "";
+} */
