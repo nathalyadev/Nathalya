e5f353eb78cb6c8795b472fbb3c955a974945761
nathgit198 <nath.conta14@gmail.com>
2022-06-13 15:20:50 -0300

    Preloader

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/* Preloader */
+    var loader = document.getElementById("preloader")
+    window.addEventListener("load", function() {
+        loader.style.display = "none"
+        document.body.style.overflow = "unset";
+    })
+
 /* Exibir e ocultar menu */
 function menu() {
     var menuLua = document.querySelector('.darkmodeTheme');
