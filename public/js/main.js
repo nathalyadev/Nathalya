ab39b086fc2ca78e5391574205afe271c1846024
nathgit198 <ns.contato21@gmail.com>
2022-10-13 19:07:43 -0300

    Removendo arquivos desnecessários

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,18 @@
 /* Adicionar e remover secao */
 var addSection = document.querySelector(".addSection");
-
 addSection.addEventListener("click", function() {
     var home = document.getElementById("home");
-    home.style.display = "block";
+    var page1 = document.querySelector(".page1");
+    if (home.style.display = "block") {
+        page1.style.display = "none";
+    } 
 })
 
-
 var deleteSection = document.querySelector(".homeFooter");
-
 deleteSection.addEventListener("click", function() {
     var home = document.getElementById("home");
-    home.style.display = "none";    
+    var page1 = document.querySelector(".page1");
+    if (home.style.display = "none") {
+        page1.style.display = "block";
+    } 
 })
